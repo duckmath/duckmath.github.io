70bc96db08da569ac7a1b8269d57f68900da750e
maddox <maddox@server.fake>
2023-02-24 12:35:06 -0500

    test ruffle and wasm

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,16 @@
+var swfobject = {};
+
+swfobject.embedSWF = function(url, cont, width, height){
+    var ruffle = window.RufflePlayer.newest(),
+        player = Object.assign(document.getElementById(cont).appendChild(ruffle.createPlayer()), {
+            width: width,
+            height: height,
+            style: 'width: ' + width + 'px; height: ' + height + 'px',
+        });
+
+    player.load({ url: url });
+}
+
 
 function enlargeimage(image) {
     image.style.transform = "scale(1.15)";
