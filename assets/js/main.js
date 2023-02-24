543123c70e5f0b21711e1a8ad8d20d416e4601c5
maddox3 <maddox3fake@admin>
2023-02-24 17:46:10 -0500

    fixed test with ruffle cuz a bunch of inherit errors

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,6 +12,7 @@ swfobject.embedSWF = function(url, cont, width, height){
 }
 
 
+
 function enlargeimage(image) {
     image.style.transform = "scale(1.15)";
     image.style.transition = "transform 0.25s ease";
