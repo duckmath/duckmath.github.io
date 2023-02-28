58a98b4743d545edd4f48d9f4ba3443cc49ea67c
maddox3 <maddox3fake@admin>
2023-02-28 17:37:59 -0500

    Main push before adding most games

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,18 +3,18 @@ const template = document.createElement('template');
 template.innerHTML = `
   <div class ="topnav">
     <div class="dropdown">
-        <a class = "topnavbutton">More</a>
+        <a class = "topnavbutton">MORE</a>
         <div class="dropdown-content">
             <a href="#">List 1</a>
             <a href="#">List 2</a>
             <a href="#">List 3</a>
         </div>
     </div>
-    <a href ="/about.html">About</a>
-    <a href ="/changelog.html">Changelog</a>
-    <a href ="/leaderboard.html">Leaderboard</a>
-    <a href ="/games.html">Games</a>
-    <a href="/index.html">Home</a>
+    <a href ="/about.html">ABOUT</a>
+    <a href ="/changelog.html">CHANGELOG</a>
+    <a href ="/leaderboard.html">LEADERBOARD</a>
+    <a href ="/g4m3s.html">G4M3S</a>
+    <a href="/index.html">HOME</a>
 </div>
 <div class = "bottom_text">Copyright © 2022-2023 Duck Math - All Rights Reserved.</div>
 <div class ="icon">
