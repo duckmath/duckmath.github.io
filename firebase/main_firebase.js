4c34b7a7ce1c727bb586618c0ed9bf86fa23e1cb
Light Andy <andy.197197@yahoo.com>
2023-11-13 00:37:59 +0200

    Formatted the code

--- firebase/main_firebase.js
+++ firebase/main_firebase.js
@@ -1,11 +1,11 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyBzv3i9lYIhyiQrfYnELlf2OesAhKMIbdk",
-    authDomain: "duckmath-6e834.firebaseapp.com",
-    projectId: "duckmath-6e834",
-    storageBucket: "duckmath-6e834.appspot.com",
-    messagingSenderId: "239032497719",
-    appId: "1:239032497719:web:2fdd4271eddaf08897b1b2",
-    measurementId: "G-R6ZHT5H49D"
+  apiKey: "AIzaSyBzv3i9lYIhyiQrfYnELlf2OesAhKMIbdk",
+  authDomain: "duckmath-6e834.firebaseapp.com",
+  projectId: "duckmath-6e834",
+  storageBucket: "duckmath-6e834.appspot.com",
+  messagingSenderId: "239032497719",
+  appId: "1:239032497719:web:2fdd4271eddaf08897b1b2",
+  measurementId: "G-R6ZHT5H49D",
 };
 
 /*
@@ -15,125 +15,113 @@ import * as authentication from "./files/gstatic.com_firebasejs_10.5.2_firebase-
 import * as firestore from "./files/gstatic.com_firebasejs_10.5.2_firebase-firestore.js";
 */
 
-
-
-
 import * as firebase from "https://www.gstatic.com/firebasejs/10.5.2/firebase-app.js";
 //import * as gAnalytics from "https://www.gstatic.com/firebasejs/10.5.2/firebase-analytics.js";
 import * as authentication from "https://www.gstatic.com/firebasejs/10.5.2/firebase-auth.js";
 import * as firestore from "https://www.gstatic.com/firebasejs/10.5.2/firebase-firestore.js";
 
-
-
-
-
-let user_object = null
-
+let user_object = null;
 
 const app = firebase.initializeApp(firebaseConfig);
 //const analytics = gAnalytics.getAnalytics(app);
-const auth = authentication.getAuth(app)
+const auth = authentication.getAuth(app);
 const db = firestore.getFirestore(app);
 console.log("Firebase initialized");
 
-
 //Returns Boolean
-function isSignedIn(){
-    if(user_object == null){
-        return false;
-    }
-    else{
-        return true
-    }
+function isSignedIn() {
+  if (user_object == null) {
+    return false;
+  } else {
+    return true;
+  }
 }
-
-
 
 // Signs in with Google
-function googleSignIn(){
-    authentication.signInWithPopup(auth, new authentication.GoogleAuthProvider())
-        .then((result) => {
-            const user = result.user;
+function googleSignIn() {
+  authentication
+    .signInWithPopup(auth, new authentication.GoogleAuthProvider())
+    .then((result) => {
+      const user = result.user;
     })
-        .catch((error) => {
-            console.log(error)
-    })
-
+    .catch((error) => {
+      console.log(error);
+    });
 }
 // sign in that must be called with email and password
-function emailSignIn(email, password){
-    authentication.createUserWithEmailAndPassword(auth, email, password)
-        .then((result) => {
-        const user = result.user;
+function emailSignIn(email, password) {
+  authentication
+    .createUserWithEmailAndPassword(auth, email, password)
+    .then((result) => {
+      const user = result.user;
     })
-        .catch((error) => {
-            console.log(error)
-            emailSignIn(email, password)//bad practice but couldnt hurt ig
-        })
+    .catch((error) => {
+      console.log(error);
+      emailSignIn(email, password); //bad practice but couldnt hurt ig
+    });
 }
 // logs the user out then the authstatechanged gets run
-function logOut(){
-    authentication.signOut(auth).then(() => {
-        // Sign-out successful.
-    }).catch((error) => {
-        // An error happened.
+function logOut() {
+  authentication
+    .signOut(auth)
+    .then(() => {
+      // Sign-out successful.
+    })
+    .catch((error) => {
+      // An error happened.
     });
 }
 
-
-
-function streakPopUp(streak_object){
-    if(streak_object.firstChild.getAttribute("src") === "/assets/img/gray-streak-icon.webp" && checkUsersStreak(user_object)){ // means I'm not signed in, I can also just check user
-        console.log("You must be signed in to use streaks") // show the user this
-        googleSignIn();
-    }
-    else{
-        window.location = "/streak.html";
-    }
+function streakPopUp(streak_object) {
+  if (
+    streak_object.firstChild.getAttribute("src") ===
+      "/assets/img/gray-streak-icon.webp" &&
+    checkUsersStreak(user_object)
+  ) {
+    // means I'm not signed in, I can also just check user
+    console.log("You must be signed in to use streaks"); // show the user this
+    googleSignIn();
+  } else {
+    window.location = "/streak.html";
+  }
 }
 const login_button = document.getElementById("LoginButton");
 const logout_button = document.getElementById("LogoutButton");
 const streak_icon = document.getElementById("StreakIcon");
-if(streak_icon !== null){
-    login_button.addEventListener("click", googleSignIn);
-    logout_button.addEventListener("click", logOut);
-    streak_icon.addEventListener("click", function(){
-        streakPopUp(this)
-    });
-    console.log("Event Listeners Added")
-
+if (streak_icon !== null) {
+  login_button.addEventListener("click", googleSignIn);
+  logout_button.addEventListener("click", logOut);
+  streak_icon.addEventListener("click", function () {
+    streakPopUp(this);
+  });
+  console.log("Event Listeners Added");
+} else {
+  console.log("FATAL ERROR ICONS COULD NOT ADD JS CODE");
 }
-else {
-    console.log("FATAL ERROR ICONS COULD NOT ADD JS CODE")
-}
-
-
 
 /* For each of your app's pages that need information about the signed-in user,
 attach an observer to the global authentication object.
 This observer gets called whenever the user's sign-in state changes.
 https://firebase.google.com/docs/reference/js/auth.user
 */
-authentication.onAuthStateChanged(auth, (user) => { // do shit
-    if (user) {
-        // somehow if I came here from streaks it could know, maybe use html post req or url parameters
-        user_object = user;
-        login_button.style.display = "none";
-        logout_button.style.display = "inline"
-        streak_icon.firstChild.src ="/assets/img/streak-icon.webp"
-        console.log("Logged In")
+authentication.onAuthStateChanged(auth, (user) => {
+  // do shit
+  if (user) {
+    // somehow if I came here from streaks it could know, maybe use html post req or url parameters
+    user_object = user;
+    login_button.style.display = "none";
+    logout_button.style.display = "inline";
+    streak_icon.firstChild.src = "/assets/img/streak-icon.webp";
+    console.log("Logged In");
+  } else {
+    user_object = null;
+    login_button.style.display = "inline";
+    logout_button.style.display = "none";
+    console.log("Logged Out");
+    streak_icon.firstChild.src = "/assets/img/gray-streak-icon.webp";
 
-
-    } else {
-        user_object = null
-        login_button.style.display = "inline"
-        logout_button.style.display = "none"
-        console.log("Logged Out")
-        streak_icon.firstChild.src ="/assets/img/gray-streak-icon.webp"
-
-        //change fire icon
-
-    }
+    //change fire icon
+  }
 });
 
 /*
@@ -152,47 +140,44 @@ const user_streaks_collection = firestore.collection(db, "user_streaks");
  * @returns {boolean} True if the user has a streak, false otherwise.
  */
 async function checkUsersStreak(current_user_id) {
-    if(isSignedIn()){
-
-        const query_items = await firestore.query(user_streaks_collection, firestore.where("userID", "==", current_user_id)); // watch this line
-        try {
-
-            const querySnapshot = await firestore.getDocs(query_items);
-            if(querySnapshot.docs.length === 0){
-                console.log("No doc found")
-                return false
-            }
-            for (const doc of querySnapshot.docs) {
-                if (doc.id === current_user_id) {
-                    if(doc.data().has_streak === true){ // new line verify it works
-                        return true;
-                    }
-                }
-            }
-        } catch (error) {
-            console.error("Error querying Firestore:\n", error);
+  if (isSignedIn()) {
+    const query_items = await firestore.query(
+      user_streaks_collection,
+      firestore.where("userID", "==", current_user_id)
+    ); // watch this line
+    try {
+      const querySnapshot = await firestore.getDocs(query_items);
+      if (querySnapshot.docs.length === 0) {
+        console.log("No doc found");
+        return false;
+      }
+      for (const doc of querySnapshot.docs) {
+        if (doc.id === current_user_id) {
+          if (doc.data().has_streak === true) {
+            // new line verify it works
+            return true;
+          }
         }
+      }
+    } catch (error) {
+      console.error("Error querying Firestore:\n", error);
     }
-    return false;
+  }
+  return false;
 }
 
-
-async function testCase1(){
-
-    checkUsersStreak("MIMSxtxbGjeBBdFos5O0xXDGCjx1").then((result) => {
-        if(result === true){
-            console.log("passed test case 1")
-        }
-        else{
-            console.log("failed test case 1")
-        }
+async function testCase1() {
+  checkUsersStreak("MIMSxtxbGjeBBdFos5O0xXDGCjx1")
+    .then((result) => {
+      if (result === true) {
+        console.log("passed test case 1");
+      } else {
+        console.log("failed test case 1");
+      }
     })
     .catch((error) => {
-        console.log("failed test case 1")
+      console.log("failed test case 1");
     });
 }
 
 setTimeout(testCase1, 2000);
-
-
-
