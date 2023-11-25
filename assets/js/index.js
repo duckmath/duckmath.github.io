c5e4186c7c6a97b3e522ae68918691ffeb74233d
Light Andy <andy.197197@yahoo.com>
2023-11-25 14:58:32 +0200

    Beautified file + Fixed warnings
    Formatted both HTML and JS in this file using the Prettier extension in VS Code. Addressed HTML warnings by relocating styles from the HTML file to the CSS file, resulting in a cleaner and more organized codebase.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,52 +1,66 @@
 const templatetopnav = document.createElement("template");
 
 templatetopnav.innerHTML = `
-  <div class ="topnav">
-    <a href="#" id="LoginButton" style="color: green;">LOGIN</a>
-    <a href="#" id="LogoutButton" style="color: #D30000; display: none">LOGOUT</a>
-    <div class="dropdown">
-        <a class = "topnavbutton">MORE↓</a>
-        <div class="dropdown-content">
-            <a href="/more/Unbl0ck3rs.html">Unbl0ck3rs</a>
-            <a href="/more/Virtual_Machines.html" style="background-color: #FFFF00">Virtual Machines</a>
-            <a href="/more/chat.html" style="background-color: #0000FF">D1sc0rd</a>
-            <a href="/more/G4m3-Requests.html" style="background-color: #00FF00">G4m3 Requests</a>
-        </div>
-    </div>
-    <a href ="/about.html">ABOUT</a>
-    <a href ="/changelog.html">CHANGELOG</a>
-    <a href ="/leaderboard.html">LEADERBOARD</a>
-    <a href ="/g4m3s.html">G4M3S</a>
-    <a href="/index.html">HOME</a>
-    <!-- streak NEEDS TO BE GRAYED OUT IF INACTIVE OR NOT LOGGED IN -->
-    <a id = "StreakIcon" href="#" style="padding: 5px 2px; justify-content: center " ><img src="/assets/img/gray-streak-icon.webp" alt="Streak" width="50" height="40"></a>
+<div class="topnav">
+<a href="#" id="LoginButton">LOGIN</a>
+<a href="#" id="LogoutButton">LOGOUT</a>
+<div class="dropdown">
+  <a class="topnavbutton">MORE↓</a>
+  <div class="dropdown-content">
+    <a href="/more/Unbl0ck3rs.html">Unbl0ck3rs</a>
+    <a href="/more/Virtual_Machines.html" class="vm">Virtual Machines</a>
+    <a href="/more/chat.html" class="dis">D1sc0rd</a>
+    <a href="/more/G4m3-Requests.html" class="gr">G4m3 Requests</a>
+  </div>
+</div>
+<a href="/about.html">ABOUT</a>
+<a href="/changelog.html">CHANGELOG</a>
+<a href="/leaderboard.html">LEADERBOARD</a>
+<a href="/g4m3s.html">G4M3S</a>
+<a href="/index.html">HOME</a>
+<!-- streak NEEDS TO BE GRAYED OUT IF INACTIVE OR NOT LOGGED IN -->
+<a id="StreakIcon" href="#"
+  ><img
+    src="/assets/img/gray-streak-icon.webp"
+    alt="Streak"
+    width="50"
+    height="40"
+/></a>
 </div>
 <div id="hamburger-icon" onclick="toggleMobileMenu(this)">
-  <div class="bar1"></div>
-  <div class="bar2"></div>
-  <div class="bar3"></div>
-  <ul class="mobile-menu">
-    <li><a href="/index.html">HOME</a></li>
-    <li><a href ="/g4m3s.html">G4M3S</a></li>
-    <li><a href ="/leaderboard.html">LEADERBOARD</a></li>
-    <li><a href ="/changelog.html">CHANGELOG</a></li>
-    <li> <a href ="/about.html">ABOUT</a></li>
-    <li>
-    <a class = "topnavbutton">MORE↓</a>
+<div class="bar1"></div>
+<div class="bar2"></div>
+<div class="bar3"></div>
+<ul class="mobile-menu">
+  <li><a href="/index.html">HOME</a></li>
+  <li><a href="/g4m3s.html">G4M3S</a></li>
+  <li><a href="/leaderboard.html">LEADERBOARD</a></li>
+  <li><a href="/changelog.html">CHANGELOG</a></li>
+  <li><a href="/about.html">ABOUT</a></li>
+  <li>
+    <a class="topnavbutton">MORE↓</a>
     <div class="dropdown-content">
-        <a href="/more/Unbl0ck3rs.html">Unbl0ck3rs</a>
-        <a href="/more/Virtual_Machines.html" style="background-color: #FFFF00">Virtual Machines</a>
-        <a href="/more/chat.html" style="background-color: #0000FF">D1sc0rd</a>
-        <a href="/more/G4m3-Requests.html" style="background-color: #00FF00">G4m3 Requests</a>
+      <a href="/more/Unbl0ck3rs.html">Unbl0ck3rs</a>
+      <a href="/more/Virtual_Machines.html" class="vm">Virtual Machines</a>
+      <a href="/more/chat.html" class="dis">D1sc0rd</a>
+      <a href="/more/G4m3-Requests.html" class="gr">G4m3 Requests</a>
     </div>
-    </li>
-  </ul>
+  </li>
+</ul>
 </div>
-<div class ="icon">
-    <a href ="/index.html"><img onmouseover="spin(this)" onmouseout="unspin(this)" src="/assets/img/goose.webp" alt="Home" width="60" height="60"></a>
+<div class="icon">
+<a href="/index.html"
+  ><img
+    onmouseover="spin(this)"
+    onmouseout="unspin(this)"
+    src="/assets/img/goose.webp"
+    alt="Home"
+    width="60"
+    height="60"
+/></a>
 </div>
-
 `;
+
 function toggleMobileMenu(menu) {
   menu.classList.toggle("open");
 }
