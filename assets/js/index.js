ad372212d8414544e3ea7080ecc992aa3a3d6ddf
Osieeq12 <rohanrajesh119+github@gmail.com>
2024-11-20 17:50:21 +0000

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -20,7 +20,7 @@ templatetopnav.innerHTML = `
 <div class="centered-fixed">
      <img src="/assets/img/scaled_goose_pixel.png" onmouseover="spin(this)"
     onmouseout="unspin(this)" alt="Duck" onClick="home()" width="40px" height="40px" />
-      <span class="duckmath-header" onmouseenter="showchildren(this);" onmouseleave="hidechildren(this);">DuckMath<span class="small-text" style="visibility: hidden">.org</span></span>
+      <span class="duckmath-header" onmouseenter="showchildren(this);" onmouseleave="hidechildren(this);">g4m3s211<span class="small-text" style="visibility: hidden">.github.io</span></span>
 </div>
 
 
