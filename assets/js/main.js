f16892e80fae03b6be30c63a60229c3e2a70dc9b
maddox05 <maddoxschmidlkofer@gmail.com>
2024-11-20 20:49:04 -0500

    forgot the dot

--- assets/js/main.js
+++ assets/js/main.js
@@ -268,5 +268,5 @@ document.addEventListener("DOMContentLoaded", function () {
   const TLD = window.location.hostname.split(".")[0];
   const ending = window.location.hostname.split(".")[1];
   headerSpan.firstChild.textContent = TLD;
-  headerSpan.children[0].textContent = ending;
+  headerSpan.children[0].textContent = "." + ending;
 });
