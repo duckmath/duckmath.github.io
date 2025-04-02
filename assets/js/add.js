2544a048f2362d9cb687cb10e8d64d0eb33693cb
maddox05 <maddoxschmidlkofer@gmail.com>
2025-04-02 12:29:33 -0400

    test google ana

--- assets/js/add.js
+++ assets/js/add.js
@@ -1,14 +1,47 @@
-///
-// append google code to site if needed
-const googleCode = document.createElement("script");
-googleCode.src =
-  "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8362959866002557";
-googleCode.async = true;
-googleCode.crossOrigin = "anonymous";
+const sitename = window.location.hostname.split(".")[0];
+const googleAna = document.createElement("script");
+googleAna.async = true;
+const duckmath_tag = "G-63K8BHHS38";
+const nullxiety_tag = "G-5GCMDH3FDB";
+const quizquack_tag = "G-55QC47N5PZ";
 
-if (window.location.hostname.split(".")[0] != "duckmath") {
-  console.log("appended goog");
+console.log("Sitename:", sitename);
+
+function getTagFromSiteName() {
+  switch (window.location.hostname.split(".")[0]) {
+    case "duckmath":
+      return duckmath_tag;
+    case "nullxiety":
+      return nullxiety_tag;
+    case "quizquack":
+      return quizquack_tag;
+    default:
+      return null;
+  }
+}
+
+googleAna.src = `https://www.googletagmanager.com/gtag/js?id=${getTagFromSiteName()}`;
+
+document.head.appendChild(googleAna);
+
+window.dataLayer = window.dataLayer || [];
+function gtag() {
+  dataLayer.push(arguments);
+}
+gtag("js", new Date());
+
+gtag("config", getTagFromSiteName());
+
+if (sitename != "duckmath") {
+  ///
+  // append google code to site if needed
+  const googleCode = document.createElement("script");
+  googleCode.src =
+    "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8362959866002557";
+  googleCode.async = true;
+  googleCode.crossOrigin = "anonymous";
   document.head.appendChild(googleCode);
+  console.log("appended goog");
 } else {
   //  <link rel="dns-prefetch" href="https://universal.wgplayer.com" />
   // this should be in no matter what ^
