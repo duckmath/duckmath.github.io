4379b16c09d1f8e29d8d4f022ddbe8959325b157
maddox05 <maddoxschmidlkofer@gmail.com>
2025-05-01 19:18:26 -0400

    fix?

--- assets/js/add.js
+++ assets/js/add.js
@@ -27,54 +27,55 @@ function getTagFromSiteName() {
 }
 
 const tagId = getTagFromSiteName();
+if (tagId) {
+  googleAna.src = `https://www.googletagmanager.com/gtag/js?id=${tagId}`;
 
-googleAna.src = `https://www.googletagmanager.com/gtag/js?id=${getTagFromSiteName()}`;
+  document.head.appendChild(googleAna);
 
-document.head.appendChild(googleAna);
+  window.dataLayer = window.dataLayer || [];
+  function gtag() {
+    dataLayer.push(arguments);
+  }
+  gtag("js", new Date());
+  let adVariant = "None";
 
-window.dataLayer = window.dataLayer || [];
-function gtag() {
-  dataLayer.push(arguments);
+  if (sitename != "duckmath" || Math.random() > 0.5) {
+    ////
+    ///
+    // append google code to site if needed
+    const googleCode = document.createElement("script");
+    googleCode.src =
+      "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8362959866002557";
+    googleCode.async = true;
+    googleCode.crossOrigin = "anonymous";
+    document.head.appendChild(googleCode);
+    console.log("appended goog");
+    adVariant = "Adsense";
+  } else {
+    //  <link rel="dns-prefetch" href="https://universal.wgplayer.com" />
+    // this should be in no matter what ^
+    !(function (e, t) {
+      (a = e.createElement("script")),
+        (m = e.getElementsByTagName("script")[0]),
+        (a.async = 1),
+        (a.src = t),
+        (a.fetchPriority = "high"),
+        m.parentNode.insertBefore(a, m);
+    })(
+      document,
+      "https://universal.wgplayer.com/tag/?lh=" +
+        window.location.hostname +
+        "&wp=" +
+        window.location.pathname +
+        "&ws=" +
+        window.location.search
+    );
+    console.log("appended wee");
+    adVariant = "Weegoo";
+  }
+
+  gtag("config", tagId, {
+    m_ad_variant: adVariant,
+  });
 }
-gtag("js", new Date());
-let adVariant = "None";
-
-if (sitename != "duckmath" || Math.random() > 0.5) {
-  ////
-  ///
-  // append google code to site if needed
-  const googleCode = document.createElement("script");
-  googleCode.src =
-    "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8362959866002557";
-  googleCode.async = true;
-  googleCode.crossOrigin = "anonymous";
-  document.head.appendChild(googleCode);
-  console.log("appended goog");
-  adVariant = "Adsense";
-} else {
-  //  <link rel="dns-prefetch" href="https://universal.wgplayer.com" />
-  // this should be in no matter what ^
-  !(function (e, t) {
-    (a = e.createElement("script")),
-      (m = e.getElementsByTagName("script")[0]),
-      (a.async = 1),
-      (a.src = t),
-      (a.fetchPriority = "high"),
-      m.parentNode.insertBefore(a, m);
-  })(
-    document,
-    "https://universal.wgplayer.com/tag/?lh=" +
-      window.location.hostname +
-      "&wp=" +
-      window.location.pathname +
-      "&ws=" +
-      window.location.search
-  );
-  console.log("appended wee");
-  adVariant = "Weegoo";
-}
-
-gtag("config", tagId, {
-  m_ad_variant: adVariant,
-});
 ///
